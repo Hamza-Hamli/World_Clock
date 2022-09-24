e70ac9a62a8aba6c01cf26b465748d47d0df7c04
Hamza-Hamli <hamzah.alhamli@gmail.com>
2022-09-24 19:44:02 +0300

    digital clock fixing

--- JS/script.js
+++ JS/script.js
@@ -14,6 +14,36 @@ setInterval(() => {
     hr.style.transform = 'rotateZ(${hh+(mm/12)}deg)';
     mn.style.transform = 'rotateZ(${mm}deg)';
     sc.style.transform = 'rotateZ(${ss}deg)';
+
+
+    // digital colock 
+
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours;
+    let m = new Date().getMinutes;
+    let s = new Date().getSeconds;
+
+    // convert 24hr to 12hr
+
+    if (h > 12){
+        h = h -12;
+    }
+
+
+    // add ZERO before singel digit number
+
+    h = (h < 10)? "0" + h : h
+    m = (m < 10)? "0" + m : m
+    s = (s < 10)? "0" + s : s
+
+    // hours.innerHTML = h;
+    // minutes.innerHTML = m;
+    // seconds.innerHTML = s;
 })
+
 
 
