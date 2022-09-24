b8a5b3c6e6dd8efdcc14eb4716e7fae75ee8a38a
Hamza-Hamli <hamzah.alhamli@gmail.com>
2022-09-24 18:56:53 +0300

    linking to HTML

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,19 @@
-let d = get
 
-let hr = document.querySelector('#hr');
-let mn = document.querySelector('#mn');
-let sc = document.querySelector('#sc');
+// let hir = document.querySelector('#hr');
 
-let day = new Date();
-let hh = day.getHours() * 30;
-let mm = day.getMinutes() * 6;
-let ss = day.getSeconds() *6;
+setInterval(() => {
+    let hr = document.querySelector('#hr');
+    let mn = document.querySelector('#mn');
+    let sc = document.querySelector('#sc');
+    
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() *6;
+    
+    hr.style.transform = 'rotateZ(${hh+(mm/12)}deg)';
+    mn.style.transform = 'rotateZ(${mm}deg)';
+    sc.style.transform = 'rotateZ(${ss}deg)';
+})
 
-hr.style.transform = 'rotateZ(${hh+(mm/12)}deg)';
-mn.style.transform = 'rotateZ(${mm}deg)';
-sc.style.transform = 'rotateZ(${ss}deg)';
+
